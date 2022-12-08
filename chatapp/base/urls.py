7381f42d821882898ecfe07ce5a7b9c6aad5d2af
lidialuncian <73431951+lidialuncian@users.noreply.github.com>
2022-12-08 11:04:28 +0200

    first commit

---
+++ chatapp/base/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('room/<str:pk>/', views.room, name='room'),
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<str:pk>', views.updateRoom, name='update-room'),
+    path('delete-room/<str:pk>', views.deleteRoom, name='delete-room'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerUser, name='register'),
+    path('delete-message/<str:pk>', views.deleteMessage, name='delete-message'),
+    path('profile/<str:pk>', views.userProfile, name='user-profile')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
